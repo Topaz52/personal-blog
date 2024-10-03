2e0ba943d88ebe7db1d0b9e53842bc5e49f2ed11
Ismail Masseran <isml.msrn11@gmail.com>
2024-10-03 13:10:27 +0800

    Updated personal blog template with latest changes

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,26 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Phishing Website Detection using LSTM',
+    description: `This project leverages Long Short-Term Memory (LSTM) neural networks to 
+    identify phishing websites based on URL patterns. By training the model on features 
+    extracted from malicious and legitimate websites, it aims to accurately classify URLs as 
+    either phishing or safe. The system is integrated into a web application, allowing users 
+    to input URLs and receive real-time phishing predictions.`,
+    imgSrc: '/static/images/Project-Phishing-Detection/phishing-detection-app.png',
+    href: 'https://projects.ismailmasseran.com/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Personal Portfolio Website using Next.js',
+    description: `This project is a personal portfolio website built using Next.js, designed to showcase my 
+    skills, projects, and experiences. The website features a clean and modern interface, optimized for performance 
+    and responsive on all devices. It highlights my work in AI, machine learning, and web development, providing 
+    detailed descriptions of each project with links to code repositories and live demos. In addition to static 
+    content, the portfolio is integrated with dynamic features like blogs and project updates, ensuring the website 
+    remains current and engaging. The use of Next.js allows for fast rendering, server-side generation, and a 
+    seamless user experience, making it both highly functional and easy to maintain.`,
+    imgSrc: '/static/images/personal-portfolio-website/personal-website-img.png',
+    href: 'https://ismailmasseran.com/',
   },
 ]
 
